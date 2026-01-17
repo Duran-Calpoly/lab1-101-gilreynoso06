d8e563461b971aa866edb0dd88c1b8ed023cfdbb
Gilberto Reynoso Sandoval <greyno06@calpoly.edu>
2026-01-17 00:28:41 +0000

    Completed Lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,13 @@
-pass
+def calculate_average(num1, num2, num3): 
+    return (num1+num2+num3)/ 3
+
+def add_tax(bill_total): 
+    # tax_rate = 0.10 
+    # tax_amount = bill_total*tax_rate
+    return bill_total*1.10
+
+def greet_user(name):
+    return f"Hello {name}"
+               
+               
+
